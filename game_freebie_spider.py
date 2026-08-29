a44f68286d5ccd3f3088622299a034f6ae42897d
ICKID <ickid831003@gmail.com>
2026-08-29 18:05:45 +0800

    Update game_freebie_spider.py

--- game_freebie_spider.py
+++ game_freebie_spider.py
@@ -21,7 +21,7 @@ HEADERS = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36",
     "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
     "Accept-Language": "zh-TW,zh;q=0.9,en-US;q=0.8,en;q=0.7",
-    "Accept-Encoding": "gzip, deflate, br",
+    "Accept-Encoding": "gzip, deflate",
     "Referer": "https://freesteam.games/",
     "Connection": "keep-alive",
     "Upgrade-Insecure-Requests": "1",
